7e144d5262118503bfc36ecd5de9021d5271b076
Brinavik <157197345+Brinavik@users.noreply.github.com>
2025-03-30 14:46:46 +0800

    Update main.c

---
+++ Code/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "syntax.tab.h"
+#include "node.h"
+
+extern void yyrestart (FILE*);
+extern int yyparse (void);
+int has_error = 0;
+
+int main(int argc, char** argv) { 
+  if (argc <= 1) return 0; 
+  FILE* f = fopen(argv[1], "r"); 
+  if (!f) 
+  { 
+    perror(argv[1]); 
+    return 0; 
+  } 
+  yyrestart(f);
+  // yydebug = 1; 
+  yyparse(); 
+  if (has_error == 0) 
+    display();
+  return 0; 
+}
